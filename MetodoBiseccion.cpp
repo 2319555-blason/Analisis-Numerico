557e2d13945d1e6a38494626a3066f382b93107b
2319555-blason <163922621+2319555-blason@users.noreply.github.com>
2024-08-14 18:48:49 -0300

    Metodo de biseccion

---
+++ MetodoBiseccion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    double formula, a,b, error;
+
+    cout<<"METODO DE BISECCION"<<endl;
+    cout<<"Ingrese la formula para calcular las raices: "<<endl;
+    cin>>formula;
+
+
+
+}
